cdbbe7531001f93d5cbaa007c9ffb4e0e91eda0f
Archit Gupta <rarchitgupta@gmail.com>
2024-08-27 02:26:27 +0000

    Working local tests

--- fibonacci.py
+++ fibonacci.py
@@ -5,7 +5,7 @@ def fibonacci(n, memo={}):
     if n in memo:
         return memo[n]
 
-    if n == 1:
+    if n == 1 or n == 0:
         return n        
 
     memo[n] = fibonacci(n - 1, memo) + fibonacci(n - 2, memo)
---
+++ tests.py
@@ -0,0 +1,16 @@
+import pytest
+from fibonacci import fibonacci
+
+
+def test_fibonacci_positive():
+    # This test will pass when we test it for any postive value of n
+    fibonacci(5)
+
+
+def test_fibonacci_negative():
+    # This test fails due to the intentional error we added to the code which is the code will not run for all negative values of n
+    fibonacci(-5)
+
+
+if __name__ == "__main__":
+    pytest.main()
