d5c2257eabc54f0a3c3c2e00d81bbac25c18c23e
Archit Gupta <rarchitgupta@gmail.com>
2024-08-27 00:41:07 +0000

    Fails on negative number input

--- fibonacci.py
+++ fibonacci.py
@@ -5,11 +5,10 @@ def fibonacci(n, memo={}):
     if n in memo:
         return memo[n]
 
-    if n <= 1:
-        return n
-        # n = "This should throw an error as specified"
+    if n == 1:
+        return n        
 
-    memo[n] = fibonacci(n - 1, memo) + fibonacci(n - 2, memo) # Code fails here due to subtraction operation being performed on a string i.e. n
+    memo[n] = fibonacci(n - 1, memo) + fibonacci(n - 2, memo)
     return memo[n]
 
 
