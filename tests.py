2ad3a57729cc08bfd4adece2f6a1240fea2c2db1
Vyom Patel <104128972+vyompatel77@users.noreply.github.com>
2024-08-28 19:50:45 -0400

    Add files via upload

--- tests.py
+++ tests.py
@@ -1,16 +1,12 @@
-import pytest
+import pytest # type: ignore
 from fibonacci import fibonacci
-
 
 def test_fibonacci_positive():
     # This test will pass when we test it for any postive value of n
     fibonacci(5)
 
-
 def test_fibonacci_negative():
     # This test fails due to the intentional error we added to the code which is the code will not run for all negative values of n
-    fibonacci(-5)
-
-
+        fibonacci(-5)  
 if __name__ == "__main__":
     pytest.main()
